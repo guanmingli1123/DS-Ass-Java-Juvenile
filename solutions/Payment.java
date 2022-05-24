c24d0a325219223f0790413b8a4b1b1909f844a0
Guan Ming Li <97241252+guanmingli1123@users.noreply.github.com>
2022-05-25 02:06:29 +0800

    Add files via upload

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -4,6 +4,62 @@
  */
 package payment;
 
+public  class Transaction implements Comparable<Transaction>  {
+    long timer;
+    String id;
+    String tier;
+    Integer time;
+
+    public Transaction(long timer, String id, String tier) {
+        this.timer = timer;
+        this.id = id;
+        this.tier = tier;
+    }
+
+    public long getTimer() {
+        return timer;
+    }
+
+    public String getId() {
+        return id;
+    }
+    
+    public String getTier() {
+        return tier;
+    }
+
+    public Integer getTime() {
+        switch(tier){
+        case "PLATINUM":
+            return time = 3000;
+        case "GOLD":
+            return time = 2000;
+        case "SILVER":
+            return time = 1000;
+        case "BRONZE":
+            return time = 0;
+        case "":
+            return time = -1;
+        default:
+            break;
+        }
+        return time;
+    }
+    
+    
+    @Override
+    public int compareTo(Transaction o1) {
+        return this.getTime().compareTo(o1.getTime());
+    }
+
+    @Override
+    public String toString() {
+        return id + " "  ;
+    }
+    
+    
+}
+
 import java.util.Collections;
 import java.util.Scanner;
 import java.util.PriorityQueue;
