83c6d75cb5eb86ab5bfaccdd06ec4b32edf74661
Guan Ming Li <97241252+guanmingli1123@users.noreply.github.com>
2022-06-05 10:36:15 +0800

    Update Payment.java

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -1,114 +1,236 @@
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.PriorityQueue;
+package payment;
 
-class Transaction implements Comparable<Transaction>  {
-    long timer;
-    String id;
-    String tier;
-    Long stime;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Payment {
+    
+    static String transaction;
+    static Long timer;
+    static String id;
+    static String tier;
+    static String[] details;
+    static Transaction t;
+    static int digit1;
+    static int digit2 = 0;
+    static long timer1;
+    static long timer2;
+    static Queue q = new Queue();
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            transaction = in.nextLine();
+            if (transaction.equals("EXIT")) {
+                break;
+            } else if (transaction.equals("REBOOT")) {
+                q.clear();
+            } else {
+                details = transaction.split(" ");
+                timer = Long.parseLong(details[0]);
+                id = details[1];
+                tier = details[2];
+                t = new Transaction(timer, id, tier);
+                q.enqueue(t);
+
+                timer1 = t.getTimer();
+                if (timer1 % 1000 == 0) {
+                    digit1 = digit2;
+                } else {
+                    digit1 = (int) (timer1 % 10000 / 1000);
+                }
+
+                if (q.getSize() == 1) {
+                    digit2 = digit1;
+                }
+
+                if (digit1 != digit2) {
+                    digit2 = digit1;
+                    int size = q.getSize();
+                    if (size < 100){                        
+                        while (!q.isEmpty()){
+                            System.out.print(q.dequeue().toString() + " ");
+                        }
+                        System.out.println();
+                    }else {
+                        q.Sort();
+                        for (int x = 0; x < 100; x++) {
+                            if (!q.isEmpty()){
+                                System.out.print(q.dequeue().toString() + " ");
+                            }
+                        
+                        }
+                        System.out.println();
+                    }
+                   
+                }
+
+            }
+        }
+
+    }
+}
+
+
+
+
+class Sort {
+
+    static ArrayList<Transaction> temp;
+
+    public static void Sort(ArrayList<Transaction> t,  int left, int right) {
+        int middle = (left + right) / 2;
+        if (left < right) {
+            Sort(t,  left, middle);  //sort left half
+            Sort(t,  middle + 1, right);  //sort right half
+            SortedLists(t,  left, middle, right);
+        } 
+    }
+
+    public static void SortedLists(ArrayList<Transaction> t,  int left, int middle, int right) {
+        temp = new ArrayList<Transaction>();
+        int tempLeft = left;
+        int tempRight = middle + 1;
+        while (tempLeft <= middle && tempRight <= right)
+            if (t.get(tempLeft).getStartingTime() <= (t.get(tempRight).getStartingTime())) {
+                {
+                    temp.add(t.get(tempLeft));
+                }
+                tempLeft++;
+            } else {
+                {
+                    temp.add(t.get(tempRight));
+                }
+                tempRight++;
+            }
+
+        while (tempLeft <= middle) {
+            {
+                temp.add(t.get(tempLeft));
+            }
+            tempLeft++;
+        }
+
+        while (tempRight <= right) {
+            {
+                temp.add(t.get(tempRight));
+            }
+            tempRight++;
+        }
+        int i = left;
+        {
+            for (Transaction value : temp) {
+                t.set(i, value);
+                i++;
+            }
+        }
+    }
+    
+
+}
+
+
+class Queue {
+    
+    ArrayList<Transaction> list;
+
+    public Queue() {
+        list = new ArrayList<>();
+    }
+
+    public void enqueue(Transaction a){
+        if (list.size() == 0 ){
+            list.add(0,a);;
+        }else {
+            list.add(a);
+        }
+    }
+    
+     public Transaction dequeue(){
+        if (list.isEmpty()) return null;
+        return list.remove(0);
+    }
+
+    public void setList(ArrayList<Transaction> l){
+        Collections.copy(list, l);
+    }
+
+    public int getSize(){
+        return list.size();
+    }
+
+    public Transaction get(int i){
+        if(list.isEmpty()) return null;
+        return list.get(i);
+    }
+
+    public void Sort(){
+        Sort.Sort(list, 0, list.size()-1);
+    }
+
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+    
+    public void clear() {
+        list.clear();
+    }
+
+}
+
+class Transaction {
+
+    private long timer;
+    private String tier;
+    private String id;
+    private long startingTime;
 
     public Transaction(long timer, String id, String tier) {
         this.timer = timer;
-        this.id = id;
         this.tier = tier;
+        this.id = id;
+        calculateStartingTime(timer);
     }
-
-    public long getTimer() {
-        return timer;
-    }
-
+    
     public String getId() {
-        return id;
+        return this.id;
+    }
+
+    public long getTimer(){
+        return this.timer;
     }
     
     public String getTier() {
         return tier;
     }
 
-    public Long getStartingTime() {
-        switch(tier){
-        case "PLATINUM":
-            return stime = timer - 3000;
-        case "GOLD":
-            return stime = timer - 2000;
-        case "SILVER":
-            return stime = timer - 1000;
-        case "BRONZE":
-            return stime = timer - 0;
-        default:
-            break;
+    public void calculateStartingTime(long timer) {
+
+        switch (this.tier) {
+            case "PLATINUM":
+                startingTime = timer - 3000 ;
+                break;
+            case "GOLD":
+                startingTime = timer - 2000 ;
+                break;
+            case "SILVER":
+                startingTime = timer - 1000 ;
+                break;
+            case "BRONZE":
+                startingTime = timer - 0 ;
+                break;
         }
-        return stime;
+
     }
     
-    
-   @Override
-    public int compareTo(Transaction o1) {
-        return this.getStartingTime().compareTo(o1.getStartingTime());
+    public long getStartingTime() {
+        return this.startingTime;
     }
 
-    @Override
     public String toString() {
-        return id + " "  ;
+        return id + " ";
     }
-    
-    
-}
 
-public class Payment{
-
-    public static void main(String[] args) {
-        PriorityQueue<Transaction> q = new PriorityQueue<>();
-        String transaction;
-        String[] details;
-        long timer;
-        String id;
-        String tier;
-        int digit1=0;
-        long timer1;
-        int digit2;
-        long timer2;
-        int digit3=0;
-
-        while (true) {
-            Scanner in = new Scanner(System.in);
-            transaction = in.nextLine();
-            if (transaction.equals("EXIT")) {
-                break;
-            }
-            if(transaction.equals("REBOOT")){
-                q.clear();
-                break;
-            }
-            details = transaction.split(" ");
-            timer = Long.valueOf(details[0]);
-            id = details[1];
-            tier = details[2];
-            Transaction t1 = new Transaction(timer, id, tier);
-            if(q.peek()!=null && digit3<digit1){
-                timer1 = q.peek().getTimer();
-                digit1 = (int) (timer1 % 10000 / 1000); 
-            }
-            q.offer(new Transaction(timer, id, tier));
-            timer2 = t1.getTimer();
-            digit2 = (int) (timer2 % 10000 / 1000);
-            while(q.size()==1){
-                timer1 = timer2;
-                digit1 = (int) (timer1 % 10000 / 1000); 
-                break;
-            }
-            String ans = "";
-            if(digit2 > digit1){
-                for(int i=0;i<100;i++){
-                    if(!q.isEmpty()){
-                        Transaction t = q.poll();
-                        ans = ans + t;
-                    }
-                }
-                System.out.println(ans);
-            }
-            digit1 = digit3 = digit2;
-        }
-    }
 }
