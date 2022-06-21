3bf6188f1a2f67966d99a7a1ebcb044af026398d
Lim Hon Ting <94769901+LimHonTing@users.noreply.github.com>
2022-06-22 00:19:57 +0800

    Update Histogram.java

--- solutions/Histogram.java
+++ solutions/Histogram.java
@@ -4,20 +4,26 @@ import java.util.Scanner;
 public class Histogram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //Number of test cases
         int numberOfTestCases = scanner.nextInt();
-        for(int i=0;i<numberOfTestCases;i++){;
+        for(int i=0;i<numberOfTestCases;i++){
+            //number of data
             int numberOfDataPoints = scanner.nextInt();
+            //number of bins to put the data
             int numberOfBins = scanner.nextInt();
             int[] data = new int[numberOfDataPoints];
             for(int j=0;j<data.length;j++){
                 data[j] = scanner.nextInt();
             }
+            //Sort the data in ascending order
             Arrays.sort(data);
             int min = data[0];
             int max = data[numberOfDataPoints-1];
             int interval = (max-min)/numberOfBins;
             int[] cutsoffs = new int[numberOfBins+1];
             cutsoffs[0] = min;
+            
+            //Find the cutsoff for every bin
             for(int k=1;k< cutsoffs.length;k++){
                 int value = cutsoffs[k-1]+interval;
                 cutsoffs[k] = value;
@@ -26,6 +32,8 @@ public class Histogram {
             int j =0;
             int temp = 0;
             int[] counts = new int[numberOfBins];
+                                             
+            //Calculate the counts for every bin                                 
             for(int g=0;g<numberOfDataPoints;g++) {
                 if (data[g] < cutsoffs[k]) {
                     temp++;
@@ -39,9 +47,13 @@ public class Histogram {
                     j++;
                 }
             }
+                                             
+            //Print the cutsoffs
             for(Integer cutsoff : cutsoffs)
                 System.out.print(cutsoff + " ");
             System.out.println();
+                                             
+            //Print the count for every bin
             for(Integer count: counts)
                 System.out.print(count + " ");
             System.out.println();
